e397ffc11fc9af4b52755abd73c2f148d9368e7d
vicentejavierc <vicentecollado11@gmail.com>
2024-10-11 12:12:47 -0300

    vista login

--- proyecto.test/src/main.js
+++ proyecto.test/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router' // Importa el enrutador
 
-createApp(App).mount('#app')
+import './style.css'
+
+const app = createApp(App)
+app.use(router) // Usa el enrutador
+app.mount('#app')
