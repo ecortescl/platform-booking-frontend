b24b7627cad2f4e211d50ff9e0d1e76a6c418fbf
Jorgem53 <jorgemoraga53@gmail.com>
2024-10-11 12:24:05 -0300

    Paginas

--- proyecto.test/src/router/index.js
+++ proyecto.test/src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import About from '../views/About.vue'
+import Recuperarclave from '../views/Recuperarclave.vue'
+import Register from '../views/Register.vue'
+
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
+  { path: '/About', component: About },
+  { path: '/Recuperarclave', component: Recuperarclave },
+  { path: '/Register', component: Register },
   // Agrega más rutas aquí según sea necesario
 ]
 
